96153d338d04a211bd928eb9bf84dc57289333ca
gerryflap <gerry2211@gmail.com>
2020-09-19 18:30:00 +0200

    Some tiny changes

--- run_gol.jl
+++ run_gol.jl
@@ -3,4 +3,4 @@ Pkg.activate(".")
 
 using GameOfLife
 # GameOfLife.run(10000; width=40, height=40, pause=0, alive_chance=0.4)
-GameOfLife.run_gtk(width=800, height=600, pause=0.00001, alive_chance=0.2)
+GameOfLife.run_gtk(width=800, height=600, pause=0.001, alive_chance=0.2)
--- src/GameOfLife.jl
+++ src/GameOfLife.jl
@@ -9,19 +9,15 @@ w = ones(Float64, (3,3,1,1))
 w[2,2,1,1] = 0
 
 function next_step(field)
-    field = Float64.(field)
-    # Acquire sizes
-    s1, s2 = size(field)
-
     # Compute the counts of neighboring nodes using convolutions
     counts = conv(field, w; pad=1)
 
     # Compute the new field by comparing values in the field and counts
-    return ((field .== 1.0) .& (counts .>= 2) .& (counts .<= 3)) .| (counts .== 3)
+    return Float64.(((field .== 1.0) .& (counts .>= 2) .& (counts .<= 3)) .| (counts .== 3))
 end
 
 function init_field(width, height, alive_chance)
-    return rand(Float64,  height,width, 1, 1) .< alive_chance
+    return Float64.(rand(Float64,  height,width, 1, 1) .< alive_chance)
 end
 
 # === Code for text display ===
