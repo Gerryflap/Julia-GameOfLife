968ec15ff7c8a5166ef528a6db031f999b6ac5bb
gerryflap <gerry2211@gmail.com>
2020-09-19 16:23:01 +0200

    The big GTK update

--- src/GameOfLife.jl
+++ src/GameOfLife.jl
@@ -1,6 +1,9 @@
 module GameOfLife
 using NNlib
+using Gtk
+using Gtk.ShortNames
 
+# === Code for computing board states ===
 # Init weight
 w = ones(Float64, (3,3,1,1))
 w[2,2,1,1] = 0
@@ -21,6 +24,8 @@ function init_field(width, height, alive_chance)
     return rand(Float64,  height,width, 1, 1) .< alive_chance
 end
 
+# === Code for text display ===
+
 function show_field(field)
     s = ""
     sizes = size(field)
@@ -38,7 +43,7 @@ function show_field(field)
     println(s)
 end
 
-function run(n_steps; pause=0.0, width=30, height=30, alive_chance=0.3)
+function run_text(n_steps; pause=0.0, width=30, height=30, alive_chance=0.3)
     field = init_field(width, height, alive_chance)
     show_field(field)
 
@@ -50,4 +55,54 @@ function run(n_steps; pause=0.0, width=30, height=30, alive_chance=0.3)
         show_field(field)
     end
 end
+
+# Global variables for the GTK app
+win = nothing
+data = nothing
+pixbuf = nothing
+view = nothing
+running = true
+
+# Listener for app shutdown
+function stop_running(w)
+    global running = false
+end
+
+function gtk_show_field(field)
+    global win
+    if win == nothing
+        # Initialize GTK window and view
+        global win = Window("Game of Life", size(field)[2], size(field)[1])
+        global data = Gtk.RGB[Gtk.RGB(0,0,0) for x=1:size(field)[2], y=1:size(field)[1]];
+        global pixbuf = Pixbuf(data=data,has_alpha=false)
+        global view = Image(pixbuf);
+        push!(win,view);
+        signal_connect(stop_running, win, :destroy)
+        showall(win);
+    end
+    # Update buffer and trigger refresh
+    data[:] = Gtk.RGB[Gtk.RGB(
+                              field[y,x,1,1]*255,
+                              field[y,x,1,1]*255,
+                              field[y,x,1,1]*255)
+                      for x=1:size(field)[2], y=1:size(field)[1]];
+    G_.from_pixbuf(view, pixbuf)
+end
+
+function run_gtk(;pause=0.0, width=30, height=30, alive_chance=0.3)
+    # Runs the GTK app
+    global running
+    field = init_field(width, height, alive_chance)
+    gtk_show_field(field)
+
+    while running
+        if pause != 0.0
+            sleep(pause)
+        end
+        field = next_step(field)
+        gtk_show_field(field)
+    end
+end
+
+
 end # module
